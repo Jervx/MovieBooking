e88279f5aa0c496adbfa9a19befc6aea96659783
Jervx <louellagracechua@gmail.com>
2022-11-22 01:05:18 +0800

    Setup : +DbHelper

---
+++ app/src/main/java/com/embs/moviebooking/_utils/DatabaseHelper.java
@@ -0,0 +1,53 @@
+package com.embs.moviebooking._utils;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public DatabaseHelper(Context context) {
+        super(context, "MovieBooking.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+    }
+
+    public void checkTableExist() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String checkUserTable = "CREATE TABLE IF NOT EXISTS user ( userId INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT );";
+        // TODO DB TBLS
+        db.execSQL(checkUserTable);
+    }
+
+    public boolean dropDbs(SQLiteDatabase db, String[] dbNames) {
+        for (String dbName : dbNames)
+            db.execSQL(String.format("drop Table if exists %s", dbName));
+        return true;
+    }
+
+    public boolean truncateDbs(SQLiteDatabase db, String[] dbNames) {
+        for (String dbName : dbNames)
+            db.execSQL(String.format("DELETE FROM %s", dbName));
+        db.close();
+        return true;
+    }
+
+
+    public Cursor execRawQuery(String query, String[] args) {
+        Cursor result = null;
+        try {
+            result = this.getWritableDatabase().rawQuery(query, args);
+        } catch (Exception e) {
+        }
+        return result;
+    }
+}
--- app/src/main/java/com/embs/moviebooking/_utils/Helper.java
+++ app/src/main/java/com/embs/moviebooking/_utils/Helper.java
@@ -1,4 +1,17 @@
 package com.embs.moviebooking._utils;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class Helper {
+
+    public static String hashPassword(String toHash) {
+        String hashed = BCrypt.hashpw(toHash, BCrypt.gensalt());
+        return hashed;
+    }
+
+    public static boolean comparePassword(String word, String hash){
+        if(BCrypt.checkpw(word, hash)) return true;
+        return false;
+    }
+
 }
