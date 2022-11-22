395ed61cab19b3bc049cc66d5971046a6f9ba457
dctrrohan@gmail.com <dctrrohan@gmail.com>
2022-11-22 08:49:51 +0800

    front page and ProgressBar

--- app/src/main/java/com/embs/moviebooking/MainActivity.java
+++ app/src/main/java/com/embs/moviebooking/MainActivity.java
@@ -2,14 +2,21 @@ package com.embs.moviebooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
 
+import com.embs.moviebooking.front.front;
 import com.embs.moviebooking.login.login;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +24,18 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,  WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
-        Intent front = new Intent(getApplicationContext(), login.class);
-        startActivity(front);
+        viewFront();
     }
+
+
+    public void viewFront(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent front = new Intent(getApplicationContext(), front.class);
+                startActivity(front);
+            }
+        },2000);
+    }
+
 }
---
+++ app/src/main/java/com/embs/moviebooking/front/front.java
@@ -0,0 +1,43 @@
+package com.embs.moviebooking.front;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.embs.moviebooking.R;
+import com.embs.moviebooking.login.login;
+import com.embs.moviebooking.login.signup;
+
+public class front extends AppCompatActivity {
+    Button log,sign;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front);
+        button();
+
+
+    }
+
+    public void button(){
+        log = findViewById(R.id.log);
+        sign = findViewById(R.id.sign);
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotToLogin = new Intent(getApplicationContext(), login.class);
+                startActivity(gotToLogin);
+            }
+        });
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotToSignup = new Intent(getApplicationContext(), signup.class);
+                startActivity(gotToSignup);
+            }
+        });
+    }
+}
